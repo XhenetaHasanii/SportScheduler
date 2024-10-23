021221457a4eadaa6a7dd8b460c99a7cad4c13dd
XhenetaHasanii <xheneta.hasanni@gmail.com>
2024-10-23 14:49:16 +0200

    Initalize the project

---
+++ src/main/java/com/example/SportScheduler/SportSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.example.SportScheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SportScheduler/SportSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SportScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
